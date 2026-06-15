2916c4200a152342976d581c5cfdd1a678b797c8
jeevananthams3113 <santhoshkumar.s2023cse@sece.ac.in>
2026-06-15 18:01:39 +0530

    Problem 8

---
+++ binarytodecimal.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class binarytodecimal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int num=Integer.parseInt(s,2);
+        System.out.println(num);
+
+    }
+}
