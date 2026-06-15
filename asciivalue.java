3db3c71e3d650137f1a1434c9002954be87d887d
jeevananthams3113 <santhoshkumar.s2023cse@sece.ac.in>
2026-06-15 18:21:56 +0530

    Problem 12

---
+++ asciivalue.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class asciivalue {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        char c=sc.next().charAt(0);
+        int ascii=c;
+        System.out.println("Ascii Value: "+ascii);
+    }
+}
