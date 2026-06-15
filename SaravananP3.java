840d2db540f6a6e2cb303f739cce9671f2239fd0
Saravanank2005 <saravanan.k2023it@sece.ac.in>
2026-06-15 22:43:32 +0530

    Area and perimeter of a circle

---
+++ SaravananP3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SaravananP3 {
+    public static void main(String[] args) {
+        Scanner io = new Scanner(System.in);
+        System.out.print("Input the radius of the circle: ");
+        double radius = io.nextDouble();
+        System.out.println("Perimeter is = " + (2 * Math.PI * radius));
+        System.out.println("Area is = " + (Math.PI * radius * radius));
+    }
+}
