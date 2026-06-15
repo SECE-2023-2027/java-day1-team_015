7ddfc3592c4af81ce27c7b0677a8deb9ba8e1ec6
jeevananthams3113 <santhoshkumar.s2023cse@sece.ac.in>
2026-06-15 18:33:07 +0530

    Problem 15

---
+++ sameunitdigit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sameunitdigit {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if((a%10==b%10) || (a%10==c%10) || (b%10==c%10))
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
